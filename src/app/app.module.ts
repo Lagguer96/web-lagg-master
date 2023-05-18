77abe89072d4ab23238b331a1b2eea52be12f03a
Lagguer96 <medina17796@gmail.com>
2023-05-18 16:49:01 -0400

    add-commit-app-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
